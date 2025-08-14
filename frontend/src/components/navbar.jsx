1357e3019ffc0744f87005c8f1cbea0962cdaaad
Jan <larocojanlorenz@gmail.com>
2025-08-15 00:39:41 +0800

    added routes in frontend

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+ import React from 'react'
+ 
+ export default function navbar() {
+   return (
+     <div>
+        <button>Login</button>
+       <button>Sign Up</button>
+     </div>
+   )
+ }
+ 
