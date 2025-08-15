f006712932122ccea64d4e46e046e497378dcc20
Jan <larocojanlorenz@gmail.com>
2025-08-15 15:03:43 +0800

    feat: added register

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>LandingPage</h1>
+    <div className='flex flex-col justify-center items-center bg-amber-100 h-[90vh]'>
+      <h1>WELCOME TO MEDITRACK</h1>
+      <button className="bg-blue-500 rounded-md p-1 pl-3 cursor-pointer">
+          Get Started <FontAwesomeIcon icon={faChevronRight} /> 
+      </button>
     </div>
   )
 }
