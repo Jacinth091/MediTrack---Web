31b555874e534245aa4385cba2f1d95a5c218323
Jacinth <jacinthcedricbarral@gmail.com>
2025-08-16 22:46:41 +0800

    feat[Barral]: changed the input and select field to handle multiple props

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
 
-export default function InputField({ label, type = "text", ...props }) {
+export default function InputField({ label, type, ...props }) {
   return (
     <div>
-       <div className="space-y-1">
-      <label>{label}:</label>
-      <input type={type} placeholder="Type here..." {...props} />
+      <div className="space-y-1">
+        <label>{label}:</label>
+        <input
+          type={type}
+          placeholder="Type here..."
+          className="w-1/2 p-2 border rounded-lg focus:ring-2 focus:ring-blue-500 bg-white"
+          {...props}
+        />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
+
--- frontend/src/components/SelectField.jsx
+++ frontend/src/components/SelectField.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
 
 export default function SelectField({ label, options, ...props }) {
   return (
     <div>
-       <div className="space-y-1">
-      <label>{label}:</label>
-      <select {...props}>
-        <option value="">Select {label.toLowerCase()}</option>
-        {options.map(opt => <option key={opt} value={opt.toLowerCase()}>{opt}</option>)}
-      </select>
+      <div className="space-y-1">
+        <label>{label}:</label>
+        <select {...props}>
+          <option value="">Select {label.toLowerCase()}</option>
+          {options.map(opt => (
+            <option key={opt} value={opt.toLowerCase()}>
+              {opt}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
