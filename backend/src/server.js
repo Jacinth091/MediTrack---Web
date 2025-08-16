bcc7db27b85a331e1c7bcf29551282074c3d54c3
Jacinth <jacinthcedricbarral@gmail.com>
2025-08-16 22:45:30 +0800

    feat[Barral]: Added cors and body parses dependencies to get data from client to server, added routes in the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,25 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import DepartmentRoute from '../src/Routes/department.route.js';
 import Patientroute from './Routes/patient.route.js';
 import RegisterRoute from './Routes/register.route.js';
-import connectDB from './config/db.js';
+import { connectDB } from './config/db.js';
+
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 connectDB();
+
+
 app.use('/api', RegisterRoute);
+app.use('/api', DepartmentRoute)
 app.use('/api',Patientroute);
 
 app.listen(PORT, () => {
