be32a0bf7f71c09bc30f3218d000b62725b56b63
Jacinth <jacinthcedricbarral@gmail.com>
2025-08-18 16:31:36 +0800

    feat[Barral]: Added dependencies jsonwebtoken, cookie-parser, bcrypt

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,8 +2,9 @@ import mongoose from 'mongoose';
 
 export const connectDB = async() => {
   try {
+    mongoose.connection.on('connected', ()=> console.log("MediTrack is now connected."))
     await mongoose.connect(process.env.MONGO_URI)
-    console.log("Hoi amaw na connect nang database!");
+    // console.log("Hoi amaw na connect nang database!");
   } catch (error) {
     console.error("Bombo!! guba imo server: ", error);
     process.exit(1); // Exit the process with failure
