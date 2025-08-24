27d4acf5ab21524b52240a51499b17d6cb90d9ca
Jan <larocojanlorenz@gmail.com>
2025-08-24 21:11:03 +0800

    feat: added navbar

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -3,7 +3,6 @@ export default function InputField({ label, type, ...props }) {
   return (
     <div>
       <div className="space-y-1">
-        <label>{label}:</label>
         <input
           type={type}
           placeholder="Type here..."
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -44,24 +44,27 @@ const childVariants = {
     }
   };
   return (
-   <div className='grid grid-cols-2 min-h-[80vh] font-sans'>
+   <div className='flex justify-evenly items-center  min-h-[85vh] '>
       {/* <div style={{ width: '100%', height: '600px', position: 'relative' }}>
         <DarkVeil />
       </div> */}
-    <motion.div className='flex flex-col justify-center p-12 text-left space-y-6  ml-24'
+    <motion.div className='flex w-1/2 flex-col justify-center p-12 text-left space-y-6  ml-24'
      initial="initial"
       animate="animate"
       variants={containerVariants}
     >
-      <h1 className='text-5xl font-bold font-sans text-gray-900 leading-tight'>
-        WELCOME TO MEDITRACK
+      <h1 className='text-5xl font-bold font-lobster text-gray-900 leading-tight'>
+        Welcome to MediTrack
       </h1>
       
       <p className='text-xl font-light text-gray-600 leading-relaxed'>
         The First Blockchain-Encrypted, patient-centric platform that unifies your entire medical journey – across hospitals, labs, and pharmacies – in one click. Unlike traditional EHRs, MediTrack gives you zero-trust encrypted control over who accesses your records – with military-grade security and private keys you hold.
       </p>
-            <h1 className='font-serif'>Your health, our priority</h1>
+      <h1 className=' font-info font-bold text-blue-800'>Your health, our priority</h1>
 
+      {/* <h1 className="font-bold bg-gradient-to-r from-[#144D94] to-yellow-500 text-transparent bg-clip-text">
+  Your health, our priority
+</h1> */}
       <Link to="/login" className='self-start mt-4'>
         <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg px-6 py-3 transition-all duration-200 flex items-center gap-2 cursor-pointer">
           Get Started <FontAwesomeIcon icon={faChevronRight} size="sm" />
@@ -69,12 +72,12 @@ const childVariants = {
       </Link>
     </motion.div>
 
-    <motion.div className='flex justify-start items-center ml-10'
+    <motion.div className='w-1/2 flex justify-center items-center ml-10'
      initial="initial"
       animate="animate"
       variants={secondVariants}
     > 
-      <div className='w-full '>
+      <div className='w-full flex items-center justify-center '>
         <HeartBeat />
       </div>
     
