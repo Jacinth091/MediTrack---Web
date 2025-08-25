90b0043e9a4245451ce2c6da03155d59a3d43936
Jacinth <jacinthcedricbarral@gmail.com>
2025-08-25 13:32:06 +0800

    feat[Barral]: Changed the patient route to api/patient/ to seperate the route from other routes

--- backend/src/Routes/patient.route.js
+++ backend/src/Routes/patient.route.js
@@ -1,9 +1,22 @@
 import express from 'express';
-import { getAllPatients } from '../Controllers/patient.controller.js';
+import {
+  createPatient,
+  deletePatientById,
+  getAllPatients,
+  getPatientById,
+  updatePatientById
+} from '../Controllers/patient.controller.js';
 const router = express.Router();
 
 router.get('/patient', await getAllPatients);
 
-// router.get('/patient/:id', 
+router.get('/patient/:id', await getPatientById);
+
+router.post('/add-patient', await createPatient);
+
+router.put('/update-patient/:id', await updatePatientById);
+
+router.delete('/delete-patient/:id', await deletePatientById)
+
 
 export default router;
